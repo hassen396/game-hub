dbe9472f655bc1bc6b84f4a279bbec69894a67e5
smileX <120062084+hassen396@users.noreply.github.com>
2025-10-20 10:10:17 +0300

    refactor(ui): handle game with no image

--- src/Services/image-url.ts
+++ src/Services/image-url.ts
@@ -1,8 +1,9 @@
+import NoImage from "../assets/No_Image_Available.jpg";
 const getCropedImageUrl = (url: string) => {
-  if (!url) return "";
-    const target = 'media/'
-  const index = url.indexOf(target)+ target.length;
-  return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
+  if (!url) return NoImage;
+  const target = "media/";
+  const index = url.indexOf(target) + target.length;
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
 };
 
 export default getCropedImageUrl;
